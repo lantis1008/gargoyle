177c29ab3f49e7d2c809247a69e5021162bc7704
Jeremy Zimmerman <stopspazzing@gmail.com>
2016-07-11 21:59:33 -0700

    Cleanup of code layout

--- package/gargoyle/files/www/dhcp.sh
+++ package/gargoyle/files/www/dhcp.sh
@@ -7,7 +7,7 @@
 	# See http://gargoyle-router.com/faq.html#qfoss for more information
 	eval $( gargoyle_session_validator -c "$COOKIE_hash" -e "$COOKIE_exp" -a "$HTTP_USER_AGENT" -i "$REMOTE_ADDR" -r "login.sh" -t $(uci get gargoyle.global.session_timeout) -b "$COOKIE_browser_time"  )
 	gargoyle_header_footer -h -s "connection" -p "dhcp" -c "internal.css" -j "gs_sortable.js table.js dhcp.js" -z "dhcp.js" network wireless dhcp firewall
-	subnet=$(ifconfig br-lan | awk 'BEGIN {FS=":"}; $0 ~ /inet.addr/ {print $2}' | awk 'BEGIN {FS="."}; {print $1"\."$2"\."$3"\."}')
+	subnet=$(ifconfig br-lan | awk "BEGIN {FS=":"}; $0 ~ /inet.addr/ {print $2}" | awk "BEGIN {FS="."}; {print $1"\."$2"\."$3"\."}")
 %>
 
 <script>
@@ -19,19 +19,19 @@
 
 	echo "var hostData = new Array();"
 	if [ -e /etc/hosts ] ; then
-		awk ' $0 ~ /^[\t ]*[0-9]/ {print "hostData.push([\""$1"\",\""$2"\"]);"};' /etc/hosts
+		awk " $0 ~ /^[\t ]*[0-9]/ {print "hostData.push([\""$1"\",\""$2"\"]);"};" /etc/hosts
 	fi
 
 	echo "";
 	echo "var etherData = new Array();";
 	if [ -e /etc/ethers ] ; then
-		awk ' $0 ~ /^[\t ]*[0-9abcdefABCDEF]/ {print "etherData.push([\""$1"\",\""$2"\"]);"};' /etc/ethers
+		awk " $0 ~ /^[\t ]*[0-9abcdefABCDEF]/ {print "etherData.push([\""$1"\",\""$2"\"]);"};" /etc/ethers
 	fi
 
 	echo "";
 	echo "var leaseData = new Array();";
 	if [ -e /tmp/dhcp.leases ] ; then
-		awk ' $0 ~ /[a-z,A-Z,0-9]+/ {print "leaseData.push([\""$2"\",\""$3"\",\""$4"\"]);"};' /tmp/dhcp.leases
+		awk " $0 ~ /[a-z,A-Z,0-9]+/ {print "leaseData.push([\""$2"\",\""$3"\",\""$4"\"]);"};" /tmp/dhcp.leases
 	fi
 
 %>
@@ -49,7 +49,7 @@ for (etherIndex in etherData)
 	ether=etherData[etherIndex];
 	mac=ether[0].toUpperCase();
 	ip=ether[1];
-	host= ipHostHash[ip] == null ? '-' : ipHostHash[ip];
+	host= ipHostHash[ip] == null ? "-" : ipHostHash[ip];
 	staticIpTableData.push([host, mac, ip]);
 }
 
@@ -64,32 +64,32 @@ for (etherIndex in etherData)
 		<div class="panel panel-default">
 			<div class="panel-body">
 
-		<div id='dhcp_enabled_container'>
-			<div class='form-group form-inline'>
-				<input type='checkbox' id='dhcp_enabled' onclick="setEnabled(this.checked)" />
-				<label id='dhcp_enabled_label' for='dhcp_enabled'><%~ dhcp.SrvE %></label>
+		<div id="dhcp_enabled_container">
+			<div class="form-group form-inline">
+				<input type="checkbox" id="dhcp_enabled" onclick="setEnabled(this.checked)" />
+				<label id="dhcp_enabled_label" for="dhcp_enabled"><%~ dhcp.SrvE %></label>
 			</div>
 		</div>
 
-		<div id='dhcp_range_container' class='form-group form-inline'>
-			<label for='dhcp_start'><%~ Drng %>:</label>
+		<div id="dhcp_range_container" class="form-group form-inline">
+			<label for="dhcp_start"><%~ Drng %>:</label>
 		</div>
 
-		<div id='dhcp_start_container' class='form-group form-inline'>
-			<label for='dhcp_start' id='dhcp_start_label'><%~ Strt %>:</label>
-			<span class='rightcolumn'><% echo -n "$subnet" %></span>
-			<input type='text' class='form-control' id='dhcp_start' onkeyup='proofreadNumeric(this)' size='5' maxlength='3' />
+		<div id="dhcp_start_container" class="form-group form-inline">
+			<label for="dhcp_start" id="dhcp_start_label"><%~ Strt %>:</label>
+			<span class="rightcolumn"><% echo -n "$subnet" %></span>
+			<input type="text" class="form-control" id="dhcp_start" onkeyup="proofreadNumeric(this)" size="5" maxlength="3" />
 		</div>
 
-		<div id='dhcp_end_container' class='form-group form-inline'>
-			<label for='dhcp_end' id='dhcp_end_label'><%~ End %>:</label>
-			<span class='rightcolumn'><% echo -n "$subnet" %></span>
-			<input type='text' class='form-control' id='dhcp_end' onkeyup='proofreadNumeric(this)' size='5' maxlength='3' />
+		<div id="dhcp_end_container" class="form-group form-inline">
+			<label for="dhcp_end" id="dhcp_end_label"><%~ End %>:</label>
+			<span class="rightcolumn"><% echo -n "$subnet" %></span>
+			<input type="text" class="form-control" id="dhcp_end" onkeyup="proofreadNumeric(this)" size="5" maxlength="3" />
 		</div>
 
-		<div id='dhcp_lease_container' class='form-group form-inline'>
-			<label for='dhcp_lease' id='dhcp_lease_label'><%~ LsTm %>:</label>
-			<input type='text' class='form-control' onkeyup='proofreadNumeric(this)' id='dhcp_lease' size='5' maxlength='4' />
+		<div id="dhcp_lease_container" class="form-group form-inline">
+			<label for="dhcp_lease" id="dhcp_lease_label"><%~ LsTm %>:</label>
+			<input type="text" class="form-control" onkeyup="proofreadNumeric(this)" id="dhcp_lease" size="5" maxlength="4" />
 			<em>(<%~ hours %>)</em>
 		</div>
 
@@ -107,19 +107,19 @@ for (etherIndex in etherData)
 			</div>
 			<div class="panel-body">
 
-		<div id='block_mismatches_container' class='form-group form-inline'>
+		<div id="block_mismatches_container" class="form-group form-inline">
 			<div>
-				<input type='checkbox' id='block_mismatches' />
-				<label id='block_mismatch_label' for='block_mismatches'><%~ BlckM %></label>
+				<input type="checkbox" id="block_mismatches" />
+				<label id="block_mismatch_label" for="block_mismatches"><%~ BlckM %></label>
 			</div>
 		</div>
 
-		<div id='staticip_add_heading_container' class='form-group form-inline'>
-			<label class='nocolumn' id='staticip_add_heading_label' style='text-decoration:underline'><%~ AdSIP %>:</label>
+		<div id="staticip_add_heading_container" class="form-group form-inline">
+			<label class="nocolumn" id="staticip_add_heading_label" style="text-decoration:underline"><%~ AdSIP %>:</label>
 		</div>
 
-		<div class='form-group form-inline'>
-			<div id='staticip_add_container'>
+		<div class="form-group form-inline">
+			<div id="staticip_add_container">
 				<%in templates/static_ip_template %>
 			</div>
 			<div>
@@ -129,12 +129,12 @@ for (etherIndex in etherData)
 			</div>
 		</div>
 
-		<div id='staticip_table_heading_container' class='form-group form-inline'>
-			<span class='nocolumn'><%~ AsSIP %>:</span>
+		<div id="staticip_table_heading_container" class="form-group form-inline">
+			<span class="nocolumn"><%~ AsSIP %>:</span>
 		</div>
 
-		<div class='form-group form-inline'>
-			<div id='staticip_table_container' class="bottom_gap"></div>
+		<div class="form-group form-inline">
+			<div id="staticip_table_container" class="bottom_gap"></div>
 		</div>
 
 	</div>
@@ -143,15 +143,15 @@ for (etherIndex in etherData)
 </div>
 
 	<div id="firefox3_bug_correct" style="display:none">
-		<input type='text' value='firefox3_bug' />
+		<input type="text" value="firefox3_bug" />
 	</div>
 
 	<div id="bottom_button_container">
-	  <button id="save_button" class="btn btn-primary" onclick='saveChanges()'><%~ SaveChanges %></button>
-	  <button id="reset_button" class="btn btn-danger" onclick='resetData()'><%~ Reset %></button>
+	  <button id="save_button" class="btn btn-primary" onclick="saveChanges()"><%~ SaveChanges %></button>
+	  <button id="reset_button" class="btn btn-danger" onclick="resetData()"><%~ Reset %></button>
 	</div>
 
-<!-- <br /><textarea style="margin-left:20px;" rows=30 cols=60 id='output'></textarea> -->
+<!-- <br /><textarea style="margin-left:20px;" rows=30 cols=60 id="output"></textarea> -->
 
 <script>
 <!--
