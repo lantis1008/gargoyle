4a5db3149c0f4c9396199ff30097647b550ef6cf
Michael Gray <michael.gray@lantisproject.com>
2017-07-08 17:54:28 +1000

    Update print_quotas

--- package/gargoyle-firewall-util/src/print_quotas.c
+++ package/gargoyle-firewall-util/src/print_quotas.c
@@ -1,8 +1,9 @@
-/*  print_quotas --	Used to print quota data from iptables rules that use the "bandwidth" module
+/*  print_quotas --	Used to print quota data from iptables rules that use the "bandwidth" and "timemon" modules
  *  			Originally designed for use with Gargoyle router firmware (gargoyle-router.com)
  *
  *
  *  Copyright © 2009-2010 by Eric Bishop <eric@gargoyle-router.com>
+ *  Copyright © 2017 by Michael Gray <michael.gray@lantisproject.com>
  *
  *  This file is free software: you may copy, redistribute and/or modify it
  *  under the terms of the GNU General Public License as published by the
@@ -27,6 +28,7 @@
 #include <erics_tools.h>
 #include <uci.h>
 #include <ipt_bwctl.h>
+#include <ipt_tmctl.h>
 #define malloc safe_malloc
 #define strdup safe_strdup
 
@@ -41,6 +43,7 @@ int main(void)
 	struct uci_context *ctx = uci_alloc_context();
 	list* quota_sections = get_all_sections_of_type(ctx, "firewall", "quota");
 	unlock_bandwidth_semaphore_on_exit();
+	unlock_timemon_semaphore_on_exit();
 
 	/* for each ip have uint64_t[6], */
 	string_map *id_ip_to_bandwidth = initialize_string_map(1);
@@ -125,26 +128,43 @@ int main(void)
 			push_list(id_to_time, dynamic_strcat(3, "quotaTimes[\"", id, "\"] = [\"\", \"\", \"\", \"always\"];"));
 		}
 
-		char* types[] = { "combined_limit", "ingress_limit", "egress_limit" };
-		char* postfixes[] = { "_combined", "_ingress", "_egress" };
+		char* types[] = { "combined_limit", "ingress_limit", "egress_limit", "time_combined_limit" };
+		char* prefixes[] = { "bw_", "bw_", "bw_", "tm_"};
+		char* postfixes[] = { "_combined", "_ingress", "_egress", "_combined" };
 		
 		
 		int type_index;
-		for(type_index=0; type_index < 3; type_index++)
+		for(type_index=0; type_index < 4; type_index++)
 		{
 			char* limit = get_uci_option(ctx, "firewall", next_quota, types[type_index]);
 			if(limit != NULL)
 			{
-				char* type_id = dynamic_strcat(2, id, postfixes[type_index]);
-				ip_bw* ip_buf;
+				char* type_id = dynamic_strcat(3, prefixes[type_index], id, postfixes[type_index]);
 				unsigned long num_ips = 0;
-				int query_succeeded = get_all_bandwidth_usage_for_rule_id(type_id, &num_ips, &ip_buf, 5000);
+				if(strstr(id, "bw_") != NULL)
+				{
+					ip_bw* ip_buf;
+					int query_succeeded = get_all_bandwidth_usage_for_rule_id(type_id, &num_ips, &ip_buf, 5000);
+				}
+				else if(strstr(id, "tm_") != NULL)
+				{
+					ip_tm* ip_buf;
+					int query_succeeded = get_all_timemon_usage_for_rule_id(type_id, &num_ips, &ip_buf, 5000);
+				}
+
 				if(query_succeeded && num_ips > 0)
 				{
 					unsigned long ip_index = 0;
 					for(ip_index = 0; ip_index < num_ips; ip_index++)
 					{
-						ip_bw next = ip_buf[ip_index];
+						if(strstr(id, "bw_") != NULL)
+						{
+							ip_bw next = ip_buf[ip_index];
+						}
+						else if(strstr(id, "tm_") != NULL)
+						{
+							ip_tm next = ip_buf[ip_index];
+						}
 						char* next_ip = NULL;
 						if(next.ip == 0)
 						{
@@ -161,20 +181,30 @@ int main(void)
 						uint64_t *bw_list = get_string_map_element(ip_to_bandwidth,next_ip);
 						if(bw_list == NULL)
 						{
-							bw_list = (uint64_t*)malloc(sizeof(uint64_t)*6);
+							bw_list = (uint64_t*)malloc(sizeof(uint64_t)*8);
 							bw_list[0] = 0;
 							bw_list[1] = 0;
 							bw_list[2] = 0;
 							bw_list[3] = 0;
 							bw_list[4] = 0;
 							bw_list[5] = 0;
+							bw_list[6] = 0;
+							bw_list[7] = 0;
 							set_string_map_element(ip_to_bandwidth, next_ip, bw_list);
 						}
 						bw_list[type_index] = 1;
-						bw_list[type_index+3] = next.bw;
-						
 						char bw_str[50];
-						sprintf(bw_str, "%lld", next.bw);
+						if(strstr(id, "bw_") != NULL)
+						{
+							bw_list[type_index+4] = next.bw;
+							sprintf(bw_str, "%lld", next.bw);
+						}
+						else if(strstr(id, "tm_") != NULL)
+						{
+							bw_list[type_index+4] = next.tm;
+							sprintf(bw_str, "%lld", next.tm);
+						}
+
 						double bw_percent;
 						double bw_limit;
 						uint64_t bw_limit_64;
@@ -194,10 +224,11 @@ int main(void)
 						double* percent_list = get_string_map_element(ip_to_percents, next_ip);
 						if(percent_list == NULL)
 						{
-							percent_list = (double*)malloc(sizeof(double)*3);
+							percent_list = (double*)malloc(sizeof(double)*4);
 							percent_list[0] = -1;
 							percent_list[1] = -1;
 							percent_list[2] = -1;
+							percent_list[3] = -1;
 							set_string_map_element(ip_to_percents, next_ip, percent_list);
 						}
 						percent_list[type_index] = bw_percent;
@@ -205,10 +236,11 @@ int main(void)
 						uint64_t* limit_list = get_string_map_element(ip_to_limits, next_ip);
 						if(limit_list == NULL)
 						{
-							limit_list = (uint64_t*)malloc(sizeof(uint64_t)*3);
+							limit_list = (uint64_t*)malloc(sizeof(uint64_t)*4);
 							limit_list[0] = -1;
 							limit_list[1] = -1;
 							limit_list[2] = -1;
+							limit_list[3] = -1;
 							set_string_map_element(ip_to_limits, next_ip, limit_list);
 						}
 						limit_list[type_index] = bw_limit_64;
@@ -216,7 +248,6 @@ int main(void)
 				}
 				free(type_id);
 				free(limit);
-
 			}
 		}
 		free(id);
@@ -298,7 +329,7 @@ int main(void)
 					int type_index;
 					printf("quotaUsed[ \"%s\" ][ \"%s\" ] = [ ", next_id, next_ip);
 					sprintf(print_comma, "");
-					for(type_index=0; type_index < 3; type_index++)
+					for(type_index=0; type_index < 4; type_index++)
 					{
 						if(!used[type_index])
 						{
@@ -306,7 +337,7 @@ int main(void)
 						}
 						else
 						{
-							printf("%s%lld", print_comma, (long long int)used[type_index+3]);
+							printf("%s%lld", print_comma, (long long int)used[type_index+4]);
 						}
 						sprintf(print_comma, ", ");
 					}
@@ -314,7 +345,7 @@ int main(void)
 
 					printf("quotaPercents[ \"%s\" ][ \"%s\" ] = [ ", next_id, next_ip);
 					sprintf(print_comma, "");
-					for(type_index=0; type_index < 3; type_index++)
+					for(type_index=0; type_index < 4; type_index++)
 					{
 						printf("%s%6.3lf", print_comma, percents[type_index]);
 						sprintf(print_comma, ", ");
@@ -323,7 +354,7 @@ int main(void)
 
 					printf("quotaLimits[ \"%s\" ][ \"%s\" ] = [ ", next_id, next_ip);
 					sprintf(print_comma, "");
-					for(type_index=0; type_index < 3; type_index++)
+					for(type_index=0; type_index < 4; type_index++)
 					{
 						printf("%s%lld", print_comma,limits[type_index]);
 						sprintf(print_comma, ", ");
@@ -425,5 +456,4 @@ char* get_option_value_string(struct uci_option* uopt)
 
 	return opt_str;
 }
-
 
